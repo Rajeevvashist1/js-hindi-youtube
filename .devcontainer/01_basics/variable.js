b126c73731397a7242eafef1317dd89561913cb9
Rajeevvashist1 <vashistrajeev2611@gmail.com>
2025-09-28 09:02:52 +0000

    containerconfigdone

---
+++ .devcontainer/01_basics/variable.js
@@ -0,0 +1,7 @@
+console.log("Rajeev");
+
+const name = rajeev
+let age = 17
+var weight = 78
+
+console.table(name, age, weight)
---
+++ basicsofjs/variable.js
@@ -0,0 +1,13 @@
+console.log("Rajeev");
+
+const accountId = 123456
+let name = "Rajeev"
+var age = 17
+let accountstate = "Haryana"
+
+
+console.log(accountId)
+console.log(name)
+console.log(age)
+console.log(accountstate)
+
